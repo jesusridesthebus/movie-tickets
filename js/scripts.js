ca514a896425d5301ad775b6a700684a452fb801
Liz <jesusridesthebus@gmail.com>
2019-04-08 22:42:33 -0700

    add additional form structure to html, add delete button function to js

--- js/scripts.js
+++ js/scripts.js
@@ -73,6 +73,11 @@ function attachContactListeners() {
 	$('ul#contacts').on('click', 'li', function() {
 		showContact(this.id);
 	});
+	$('buttons').on('click', '.deleteButton', function() {
+		addressBook.deleteContact(this.id);
+		$('show-contact').hide();
+		displayContactDetails(addressBook);
+	});
 }
 
 $(function() {
